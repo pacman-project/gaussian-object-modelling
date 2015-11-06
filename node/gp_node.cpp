c27a75d5cd4a3eee82f11c559f74c0c3e2907f31
Federico Spinelli <fspinelli@gmail.com>
2015-11-06 19:29:13 +0100

    Add gaussian process computation
    Fix compilation, add downsampled cloud for testing

--- node/gp_node.cpp
+++ node/gp_node.cpp
@@ -22,6 +22,11 @@
 #include <pacman_vision_comm/get_cloud_in_hand.h>
 // This node service
 #include <gp_regression/start_process.h>
+//GP
+#include <gp/GaussianProcess.h>
+#include <gp/SampleSet.h>
+
+using namespace gp;
 
 /**\brief Class GaussianProcessNode
  * {Wraps Gaussian process into a ROS node}
@@ -38,6 +43,7 @@ class GaussianProcessNode
             //05:18:41 PM CET -- tabjones)
             pub_cloud = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB>> ("estimated_cloud", 1);
             cloud_ptr.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
+            hand_ptr.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
         }
         /**\brief Destructor */
         virtual ~GaussianProcessNode (){}
@@ -54,6 +60,7 @@ class GaussianProcessNode
     private:
         //input point cloud
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_ptr;
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr hand_ptr;
         //Services and publishers
         ros::ServiceServer srv_start;
         ros::Publisher pub_cloud;
@@ -62,7 +69,7 @@ class GaussianProcessNode
         //callback to start process service, executes when service is called
         bool cb_start(gp_regression::start_process::Request& req, gp_regression::start_process::Response& res)
         {
-            if(req.cloud.empty()){
+            if(req.cloud_dir.empty()){
             //Request was empty, means we have to call pacman vision service to
             //get a cloud.
                 std::string service_name = nh.resolveName("/pacman_vision/listener/get_cloud_in_hand");
@@ -80,19 +87,61 @@ class GaussianProcessNode
                 }
                 //obj cloud is saved into class, while hand cloud is wasted
                 pcl::fromROSMsg (service.response.obj, *cloud_ptr);
-                //pcl::fromROSMsg (service.response.hand, *hand_ptr); //snippet to save hand too
+                pcl::fromROSMsg (service.response.hand, *hand_ptr);
             }
             else{
             //User told us to load a cloud from disk instead.
             //TODO: Add some path checks perhaps? Lets hope the path is valid
             //      for now!  (Fri 06 Nov 2015 05:03:19 PM CET -- tabjones)
-                if (pcl::io::loadPCDFile(req.cloud, *cloud_ptr) != 0){
-                    ROS_ERROR("Error loading cloud from %s",req.cloud.c_str());
+                if (pcl::io::loadPCDFile((req.cloud_dir+"/obj.pcd"), *cloud_ptr) != 0){
+                    ROS_ERROR("Error loading cloud from %s",(req.cloud_dir+"obj.pcd").c_str());
+                    return (false);
+                }
+                if (pcl::io::loadPCDFile((req.cloud_dir+"/hand.pcd"), *hand_ptr) != 0){
+                    ROS_ERROR("Error loading cloud from %s",(req.cloud_dir + "/hand.pcd").c_str());
                     return (false);
                 }
             }
             start_gp = true;
+            compute();
             return (true);
+        }
+        //gp computation
+        void compute()
+        {
+            if(!start_gp || !cloud_ptr)
+                return;
+            Vec3Seq cloud;
+            Vec targets;
+            const size_t size_cloud = cloud_ptr->size();
+            const size_t size_hand = hand_ptr->size();
+            targets.resize(size_cloud + size_hand);
+            cloud.resize(size_cloud + size_hand);
+            for(size_t i=0; i<size_cloud; ++i)
+            {
+                Vec3 point(cloud_ptr->points[i].x, cloud_ptr->points[i].y, cloud_ptr->points[i].z);
+                cloud[i]=point;
+                targets[i]=0;
+            }
+            for(size_t i=0; i<size_hand; ++i)
+            {
+                Vec3 point(hand_ptr->points[i].x, hand_ptr->points[i].y, hand_ptr->points[i].z);
+                cloud[size_cloud +i]=point;
+                targets[size_cloud+i]=1;
+            }
+            /*****  Create the model  *********************************************/
+            SampleSet::Ptr trainingData(new SampleSet(cloud, targets));
+            LaplaceRegressor::Desc laplaceDesc;
+            laplaceDesc.noise = 0.001;
+            LaplaceRegressor::Ptr gp = laplaceDesc.create();
+            printf("Regressor created %s\n", gp->getName().c_str());
+            gp->set(trainingData);
+            /*****  Query the model with a point  *********************************/
+            Vec3 q(cloud[0]);
+            const double qf = gp->f(q);
+            const double qVar = gp->var(q);
+            std::cout << "y = " << targets[0] << " -> qf = " << qf << " qVar = " << qVar << std::endl << std::endl;
+
         }
 };
 
