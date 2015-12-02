13018e29b80dbe1ec723c9af40e861ec752a7bc4
Carlos J. Rosales <cjrosales@gmail.com>
2015-12-02 20:29:08 +0100

    Updates the test_gaussian.cpp to the new API :)
    All tests as expected!

--- tests/test_gaussian.cpp
+++ tests/test_gaussian.cpp
@@ -10,26 +10,26 @@ int main( int argc, char** argv )
         /*****  Generate INPUT data  ******************************************/
         std::cout << "Generate INPUT data for a sphere..." << std::endl;
         Data cloud;
-        int ni = 6;
-        int nj = 6;
+        int ni = 10;
+        int nj = 9;
         for(int i = 0; i < ni; ++i)
         {
                 for(int j = 0; j < nj; ++j)
                 {
                         // on
-                        cloud.coord_x.push_back(1.0*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_y.push_back(1.2*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_z.push_back(1.4*sin(2*3.1416*j/nj));
+                        cloud.coord_x.push_back(10.0*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_y.push_back(10.0*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_z.push_back(10.0*sin(2*3.1416*j/nj));
                         cloud.label.push_back(0.0);
                         // inside
-                        cloud.coord_x.push_back(0.5*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_y.push_back(0.5*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_z.push_back(0.5*sin(2*3.1416*j/nj));
+                        cloud.coord_x.push_back(0.1*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_y.push_back(0.1*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_z.push_back(0.1*sin(2*3.1416*j/nj));
                         cloud.label.push_back(-1.0);
                         // outside
-                        cloud.coord_x.push_back(2.0*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_y.push_back(2.0*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
-                        cloud.coord_z.push_back(2.0*sin(2*3.1416*j/nj));
+                        cloud.coord_x.push_back(20.0*cos(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_y.push_back(20.0*sin(2*3.1416*i/ni)*cos(2*3.1416*j/nj));
+                        cloud.coord_z.push_back(20.0*sin(2*3.1416*j/nj));
                         cloud.label.push_back(1.0);
                 }
         }
@@ -63,51 +63,72 @@ int main( int argc, char** argv )
         query.coord_x = vector<double>(1, 0.68);
         query.coord_y = vector<double>(1, 0.5);
         query.coord_z = vector<double>(1, 0.5);
+        std::vector<double> f,v;
 
-        regresor.estimate(sphere, query);
+        regresor.evaluate(sphere, query, f, v);
 
-        cout << "Query value: " << endl;
+        cout << "Query value x: " << endl;
         cout << query.coord_x.at(0) << " " << query.coord_y.at(0) << " " 
                         << query.coord_z.at(0) << endl << endl;
-        cout << "Query label (post): " << endl;
-        cout << query.label.at(0) << endl << endl;
-        // cout << "Query label covariance: " << endl;
-        // cout << query.ev_x.at(0) << " " << query.ev_y.at(0) 
-                        // << " " << query.ev_z.at(0) 
-                        // << endl << endl;
+        cout << "Query function f(x): " << endl;
+        cout << f.at(0) << endl << endl;
+        cout << "Query confidence V(f(x)): " << endl;
+        cout << v.at(0) << endl << endl;
+
+        /*****  Query the model with a point  *********************************/
+        std::cout << "Query the model with a point on the surface" << std::endl;
+        f.clear();
+        v.clear();
+        query.coord_x = vector<double>(1, 10.0*cos(2*3.1416*23/77)*cos(2*3.1416*27/77));
+        query.coord_y = vector<double>(1, 10.0*sin(2*3.1416*23/77)*cos(2*3.1416*27/77));
+        query.coord_z = vector<double>(1, 10.0*sin(2*3.1416*27/77));
+
+        regresor.evaluate(sphere, query, f, v);
+
+        cout << "Query value x: " << endl;
+        cout << query.coord_x.at(0) << " " << query.coord_y.at(0) << " "
+                        << query.coord_z.at(0) << endl << endl;
+        cout << "Query function f(x): " << endl;
+        cout << f.at(0) << endl << endl;
+        cout << "Query confidence V(f(x)): " << endl;
+        cout << v.at(0) << endl << endl;
 
         /*****  Query the model with a INPUT point  ***************************/
-        std::cout << "Query the model with a INPUT point" << std::endl;
-        query.label.clear();
-        query.coord_x = vector<double>(1, 0.1);
-        query.coord_y = vector<double>(1, 0.3);
-        query.coord_z = vector<double>(1, 0.5);
+        std::cout << "Query the model with the same INPUT" << std::endl;
+        cloud.label.clear();
+        f.clear();
+        v.clear();
 
-        regresor.estimate(sphere, query);
+        regresor.evaluate(sphere, cloud, f, v);
 
-        cout << "Query same input: " << endl;
-        cout << query.coord_x.at(0) << " " << query.coord_y.at(0) << " " 
-                        << query.coord_z.at(0) << endl << endl;
-        cout << "Query label (post): " << endl;
-        cout << query.label.at(0) << endl << endl;
-        // cout << "Query label covariance: " << endl;
-        // cout << query.ev_x.at(0) << " " << query.ev_y.at(0) 
-                        // << " " << query.ev_z.at(0) 
-                        // << endl << endl;
+        cout << "Query function f(x): " << endl;
+        for(int i = 0; i < f.size(); ++i)
+        {
+
+                cout << f.at(i) << " / ";
+        }
+        cout << endl << endl;
+
+        cout << "Query confidence V(f(x)): " << endl;
+        for(int i = 0; i < f.size(); ++i)
+        {
+                cout << v.at(i) << " /  ";
+        }
+        cout << endl << endl;
 
         /*****  Test the Tangent Basis generator  *****************************/
         std::cout << "Test the Tangent Basis generator" << std::endl;
         Eigen::Vector3d z(1.0, 1.0, 1.0);
-        std::cout << "z: " << std::endl << z << std::endl;
+        std::cout << "z: " << std::endl << z << std::endl << std::endl;
 
         Eigen::Vector3d x, y;
 
         computeTangentBasis(z, x, y);
-        std::cout << "x: " << std::endl << x << std::endl;
-        std::cout << "y: " << std::endl << y << std::endl;
-        std::cout << "x*z: " << std::endl << x.dot(z) << std::endl;
-        std::cout << "y*z: " << std::endl << y.dot(z) << std::endl;
-        std::cout << "x*y: " << std::endl << x.dot(y) << std::endl;
+        std::cout << "x: " << std::endl << x << std::endl << std::endl;
+        std::cout << "y: " << std::endl << y << std::endl << std::endl;
+        std::cout << "x*z: " << std::endl << x.dot(z) << std::endl << std::endl;
+        std::cout << "y*z: " << std::endl << y.dot(z) << std::endl << std::endl;
+        std::cout << "x*y: " << std::endl << x.dot(y) << std::endl << std::endl;
 
         return 0;
 }
