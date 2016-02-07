8874b2f376a7c1bf53e59287d192cded15041062
Carlos J. Rosales <cjrosales@gmail.com>
2016-02-07 16:30:40 +0100

    Avoid over computations for this covariance function
    The pairwise Euclidean distance already returns positive values. So, there
    is no need to compute the absolute value of positive values. Return
    directly the expression instead.
    
    The same applies for the differential of the covariance function w.r.t.
    value.

--- include/gp_regression/kernels/thin_plate.hpp
+++ include/gp_regression/kernels/thin_plate.hpp
@@ -13,24 +13,18 @@ public:
 
         inline double compute(double &value)
         {
-                double pos = std::abs(value);
-                return 2*pos*pos*pos - 3*R_*value*value + R3_;
+                return 2*value*value*value - 3*R_*value*value + R3_;
         }
 
         inline double computediff(double &value)
         {
-                double pos = std::abs(value);
-
-                double out = pos*pos*(pos/value) - R_*value;
-                if( std::isnan(out) )
-                        out = R_*value;
-                // std::cout << "out( " << value << " ) = " << 6*out << std::endl;
-                return 6*out;
+                return -6*(R_ - value);
         }
 
         inline double computediffdiff(double &value)
         {
-                return 12*value - 6*R_;
+                // not implemented
+                return 0;
         }
 
         ThinPlate(double R) :
